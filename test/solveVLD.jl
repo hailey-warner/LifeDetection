a993d3f9e62f4f0e9732c4f0f9836ff71bed433d
Grace Kim <gracek1459@gmail.com>
2025-05-21 15:05:12 -0400

    small cleanups, and turing

--- test/solveVLD.jl
+++ test/solveVLD.jl
@@ -10,8 +10,9 @@ using Plots
 
 
 include("../src/common/utils.jl")
-include("../src/bayesNet.jl")
+include("../src/bayesNet_old.jl")
 include("../src/volumeLD.jl")
+include("CONOPsOrbiter.jl")
 
 include("../src/common/plotting.jl")
 include("../src/common/simulate.jl")
@@ -20,15 +21,16 @@ include("../src/common/simulate.jl")
 variable_specs = [(:l, 2), (:h, 2), (:s, 2), (:μ, 2), (:E, 2), (:m, 2), (:n, 2)]
 dependencies = [(:l, :h), (:l, :s), (:l, :μ), (:l, :E), (:l, :m), (:l, :n)]
 probability_tables = [
-    ([:l], [(l=1,) => 0.5, (l=2,) => 0.5]),
-    ([:h, :l], [(h=1, l=1) => 0.7, (h=2, l=1) => 0.3, (h=1, l=2) => 0.3, (h=2, l=2) => 0.7]),
-    ([:s, :l], [(s=1, l=1) => 0.75, (s=2, l=1) => 0.25, (s=1, l=2) => 0.25, (s=2, l=2) => 0.75]),
-    ([:μ, :l], [(μ=1, l=1) => 0.6, (μ=2, l=1) => 0.4, (μ=1, l=2) => 0.4, (μ=2, l=2) => 0.6]),
-    ([:E, :l], [(E=1, l=1) => 0.6, (E=2, l=1) => 0.4, (E=1, l=2) => 0.4, (E=2, l=2) => 0.6]),
-    ([:m, :l], [(m=1, l=1) => 0.6, (m=2, l=1) => 0.4, (m=1, l=2) => 0.3, (m=2, l=2) => 0.7]),
-    ([:n, :l], [(n=1, l=1) => 0.8, (n=2, l=1) => 0.2, (n=1, l=2) => 0.25, (n=2, l=2) => 0.75]),
+    ([:l], [(l=1,) => 0.3, (l=2,) => 0.7]),
+    ([:h, :l], [(h=1, l=1) => 0.55, (h=2, l=1) => 0.45, (h=1, l=2) => 0.45, (h=2, l=2) => 0.55]),
+    ([:s, :l], [(s=1, l=1) => 0.7, (s=2, l=1) => 0.3, (s=1, l=2) => 0.45, (s=2, l=2) => 0.55]),
+    ([:μ, :l], [(μ=1, l=1) => 0.65, (μ=2, l=1) => 0.35, (μ=1, l=2) => 0.45, (μ=2, l=2) => 0.55]),
+    ([:E, :l], [(E=1, l=1) => 0.6, (E=2, l=1) => 0.4, (E=1, l=2) => 0.49, (E=2, l=2) => 0.51]),
+    ([:m, :l], [(m=1, l=1) => 0.61, (m=2, l=1) => 0.39, (m=1, l=2) => 0.5, (m=2, l=2) => 0.5]),
+    ([:n, :l], [(n=1, l=1) => 0.99, (n=2, l=1) => 0.01, (n=1, l=2) => 0.35, (n=2, l=2) => 0.65]),
     
 ]
+
 bn = bayes_net(variable_specs, dependencies, probability_tables)
 
 HRMS = 1 #0 # 0.5e-6 # mL # organic compounds, just going to set to zero its too small
@@ -44,30 +46,14 @@ microscope = 1 # μL # 0.001 # mL # polyelectrolyte
 nanopore = 100 #10000 # μL # 10  # mL cell like morphologies
 none = 0
 
-pomdp = volumeLifeDetectionPOMDP(
-    bn=bn,
-    λ=0.95,
-    inst=7,
-    sampleVolume=500,
-    lifeStates=3,
-    surfaceAccRate=270,
-    sampleUse=[1,1,1,1,1,1,1],
-    discount=0.9
-)
+# Running CONOPS:
+rewards, accuracy = simulate_policyVLD(pomdp, "policy", "conops", n_episodes = 1, verbose = true) # SARSOP or conops or greedy
 
-# pomdp = volumeLifeDetectionPOMDP(
-#             bn=bn, 
-#             λ=0.5, 
-#             inst=7, # one extra for not choosing anything
-#             sampleVolume=300, 
-#             lifeStates = 3,
-#             surfaceAccRate = 20, 
-#             sampleUse = [HRMS, SMS, μCE_LIF, ESA, microscope, nanopore, none],
-#             discount=0.9)
-
-# transition(pomdp, 2406, 0)
+# Running SARSOP
 solver = SARSOPSolver(verbose = true, timeout=100)
 @show_requirements POMDPs.solve(solver, pomdp)
 
 policy = solve(solver, pomdp)
 plot_alpha_vectors(policy)
+rewards, accuracy = simulate_policyVLD(pomdp, policy, "SARSOP", n_episodes = 1, verbose = true) # SARSOP or conops or greedy
+plot_alpha_vectors_VLD(policy,pomdp, 0)
