b91acfc3c9bc9832f28d618cd7fc0440d33824b8
Hailey Warner <hlwarner@stanford.edu>
2025-05-29 11:23:55 -0700

    renamed file

--- test/conops.jl
+++ test/conops.jl
@@ -1,13 +1,26 @@
-function conopsOrbiter(pomdp, o, mode_acc, prev_action)
-    sampleVolume, _ = stateindex_to_state(o, pomdp.lifeStates)
+
+function conopsOrbiter(pomdp, o, modeAcc, prevAction)
+    sampleVol , lifeState = stateindex_to_state(o, pomdp.lifeStates) 
+    if modeAcc
+        if sum(pomdp.sampleUse[1:5]) > sampleVol
+            return 7, true, 0 # accumulate
+        else
+            return 1, false, 1 # stop accumulating
+        end
+    else
+        if prevAction < 5
+            if pomdp.sampleUse[prevAction+1] < sampleVol
+                return prevAction+1, false, prevAction+1
+            else
+                return 7, true, 0
+            end
+        else
+            # if sum(pomdp.sampleUse[1:5]) > sampleVol
+            #     return 1, false, 1
+            # else
+            return 7, true, 0
+            # end
+        end
+    end
     
-    if mode_acc
-        return sum(pomdp.sampleUse[1:5]) > sampleVolume ? (7, true, 0) : (1, false, 1)
-    end
-
-    if prev_action < 5 && pomdp.sampleUse[prev_action+1] < sampleVolume
-        return (prev_action + 1, false, prev_action + 1)
-    end
-
-    return (7, true, 0)
 end
